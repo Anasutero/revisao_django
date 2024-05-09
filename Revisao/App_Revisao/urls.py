d7d5c8a39af05a8bca18b86cf1e8438c8afb5b01
Ana M���ri <48643119865@>
2024-05-09 16:38:30 -0300

    Atualizacao do projeto

--- Revisao/App_Revisao/urls.py
+++ Revisao/App_Revisao/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.abre_index,name='abre_index'),
-    path('listagem', views.listagem, name='listagem'),
+    # path('', views.abre_index,name='abre_index'),
+    path('', views.listagem, name='listagem'),
+    path('editar_cadastro/<int:id>',views.editar_cadastro,name='editar_cadastro'),
+    path('excluir_pessoa/<int:id>',views.excluir_pessoa,name='excluir_pessoa')
 ]
--- Revisao/App_Revisao/views.py
+++ Revisao/App_Revisao/views.py
@@ -1,12 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.db import transaction
+from django.urls import reverse
 from .models import Pessoas
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
-
-def abre_index(request):
-    return render(request, 'index.html')
+# def abre_index(request):
+#     return render(request, 'index.html')
 
 def listagem(request):
+    mensagem = ''
+    nome = ''
+    idade = ''
+    dados = Pessoas.objects.all()
     if(request.method == 'POST'):
         nome = request.POST.get('nome')
         idade = request.POST.get('idade')
@@ -16,9 +22,47 @@ def listagem(request):
             idade=idade
         )
         gravaCadastro.save()
-
         dados = Pessoas.objects.all()
-
         mensagem=(f"Dados do formulario {nome} {idade}")
+    return render(request, 'index.html' ,{'mensagem': mensagem, 'nome':nome,'idade':idade,'dados':dados})
 
-        return render(request, 'index.html' ,{'mensagem': mensagem,'nome':nome,'idade':idade,'dados':dados})
+
+def editar_cadastro(request,id):
+    if request.method=='POST':
+        try:
+            pessoa = Pessoas.objects.get(pk=id)
+            nome = request.POST.get('nome')
+            idade = request.POST.get('idade')
+
+
+            pessoa.nome=nome
+            pessoa.idade=idade
+            pessoa.save()
+
+            return HttpResponseRedirect(reverse('listagem'))
+        
+        except ObjectDoesNotExist:
+            pass
+
+    pessoa= Pessoas.objects.get(id=id)
+
+    return render(request, 'editar.html', {'nome': pessoa.nome, 'idade': pessoa.idade, 'id': pessoa.id})
+    
+def excluir_pessoa(request,id):
+    try:
+        with transaction.atomic():
+            pessoa = Pessoas.objects.get(id=id)
+            pessoa.delete()
+
+            return HttpResponseRedirect(reverse('listagem'))
+    except Pessoas.DoesNotExist:
+        pass
+
+ 
+
+
+        
+
+
+
+    
